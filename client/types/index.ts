2c50f237b6a8c45aa77e9c38ca5e656ea2b2cf12
PoriHue <warihue@gmail.com>
2025-05-29 18:26:00 +0900

    make a type

--- client/types/index.ts
+++ client/types/index.ts
@@ -3,3 +3,17 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type Schedule = {
+  title: string;
+  poster: string;
+  postDate: Date;
+  endDate: Date;
+  description: String;
+  priorityLevel: PriorityLevel;
+};
+
+export type PriorityLevel =
+  | 'Red'
+  | 'Yellow'
+  | 'Normal';
